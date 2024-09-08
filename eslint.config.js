4d762b70aaec00453b605b30a602738363072a51
9insane9 <145199591+9insane9@users.noreply.github.com>
2024-09-08 14:03:27 +0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    },
+    overrides: [
+      {
+        files: ["tests/**/*"],
+        env: {
+          jest: true
+        }
+      }
+    ]
+  }
+];
